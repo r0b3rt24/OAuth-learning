c82072054b3755be86f1fe78361a0a8ff017d058
Robert Cao <ch.robert.26@gmail.com>
2019-01-01 22:08:50 +0800

    mongo db setup

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup")
+const mongoose = require("mongoose")
+const keys = require('./config/keys')
 
 const app = express();
+
+//connect to mongobd
+mongoose.connect(keys.mongodb.dbURI,()=>{
+    console.log("connected to the DB")
+})
 
 // set up view engine
 app.set("view engine", 'ejs');
@@ -13,6 +20,8 @@ app.use('/auth', authRoutes);
 app.get('/', (req, res)=>{
     res.render('home');
 })
+
+
 
 app.listen(3000, ()=>{
     console.log('app now listening on port 3000')
