f478e80fc06b8823c9821f2cf1007727b950e899
Robert Cao <ch.robert.26@gmail.com>
2019-01-02 16:03:06 +0800

    Add User to the database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20');
-const keys = require("./keys")
+const keys = require("./keys");
+const User = require("../models/user-model")
 passport.use(
     new googleStrategy({
         // options for the google strategy
@@ -10,4 +11,11 @@ passport.use(
     }, (accessToken, refreshToken, profile, done)=>{
         // passport call back function
         console.log(profile);
+        // create new User
+        new User({
+            username: profile.displayName,
+            googleid: profile.id
+        }).save().then((newUser)=>{
+            console.log('new User created: '+newUser);
+        })
     }));
--- models/user-model.js
+++ models/user-model.js
@@ -8,3 +8,5 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model('user', userSchema);
+
+module.exports = User;
