fca31443d10ce57489437a562b54f55a342654e3
Robert Cao <ch.robert.26@gmail.com>
2019-01-02 22:19:04 +0800

    cookie session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup")
 const mongoose = require("mongoose")
 const keys = require('./config/keys')
+const cookieSession = require("cookie-session")
+const passport = require('passport');
 
 const app = express();
 
@@ -10,6 +12,15 @@ const app = express();
 mongoose.connect(keys.mongodb.dbURI,()=>{
     console.log("connected to the DB")
 })
+
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey],
+}))
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set up view engine
 app.set("view engine", 'ejs');
